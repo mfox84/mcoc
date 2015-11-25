1f817e00527076f0cff2885a7da3cc9cebbfbfb7
Dragon013 <the.dragon013@googlemail.com>
2015-11-25 06:38:10 +0100

    reale User und erste Ergebnisse eingetragen, Tabelle userteam gelöscht

--- db/mcoc.sql
+++ db/mcoc.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 23. Nov 2015 um 07:35
+-- Erstellungszeit: 25. Nov 2015 um 06:38
 -- Server Version: 5.6.16
 -- PHP-Version: 5.5.11
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `aq` (
   `aq_points` int(10) unsigned DEFAULT NULL,
   `aq_result` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`aq_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Daten für Tabelle `aq`
@@ -41,7 +41,8 @@ CREATE TABLE IF NOT EXISTS `aq` (
 
 INSERT INTO `aq` (`aq_id`, `aq_start`, `aq_end`, `aq_points`, `aq_result`) VALUES
 (1, 1446678000, 1447109999, 9500000, 75),
-(2, 1447887600, 1448319599, NULL, NULL);
+(2, 1447887600, 1448319599, NULL, NULL),
+(3, 1447887600, 1448319599, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -150,47 +151,43 @@ CREATE TABLE IF NOT EXISTS `useraqres` (
   KEY `uaqres_aq_id` (`uaqres_aq_id`),
   KEY `uaqres_aqday_id` (`uaqres_aqday_id`),
   KEY `uaqres_user_id` (`uaqres_user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1302 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1492 ;
 
 --
 -- Daten für Tabelle `useraqres`
 --
 
 INSERT INTO `useraqres` (`uaqres_id`, `uaqres_aq_id`, `uaqres_aqday_id`, `uaqres_user_id`, `uaqres_team`, `uaqres_points`) VALUES
-(1298, 1, 1, 1, 3, 5345),
-(1301, 1, 1, 2, 1, 534),
-(1300, 1, 1, 3, 2, 535),
-(1299, 1, 1, 4, 2, 3435),
-(1297, 1, 1, 5, 3, 354),
-(1275, 1, 2, 1, 3, 5675),
-(1278, 1, 2, 2, 1, 5453),
-(1277, 1, 2, 3, 2, 465),
-(1276, 1, 2, 4, 1, 4543),
-(1274, 1, 2, 5, 3, 5657),
-(1273, 1, 2, 8, 2, 3333),
-(1281, 1, 3, 1, 3, 1),
-(1284, 1, 3, 2, 1, 5),
-(1283, 1, 3, 3, 2, 123),
-(1282, 1, 3, 4, 2, 177554),
-(1280, 1, 3, 5, 1, 3864),
-(1279, 1, 3, 8, 1, 555),
-(1287, 1, 4, 1, 3, 1),
-(1290, 1, 4, 2, 1, 5),
-(1289, 1, 4, 3, 2, 4),
-(1288, 1, 4, 4, 3, 4),
-(1286, 1, 4, 5, 2, 5),
-(1285, 1, 4, 8, 2, 2),
-(1293, 1, 5, 1, 3, 2),
-(1296, 1, 5, 2, 1, 5),
-(1295, 1, 5, 3, 2, 7),
-(1294, 1, 5, 4, 2, 4),
-(1292, 1, 5, 5, 0, 66),
-(1291, 1, 5, 8, 2, 1),
-(875, 2, 1, 5, 0, 111),
-(874, 2, 1, 8, 3, 555),
-(876, 2, 2, 8, 1, 555),
-(877, 2, 3, 8, 0, 555),
-(878, 2, 4, 8, 0, 55);
+(1475, 1, 1, 22, 1, 150769),
+(1483, 1, 1, 23, 3, 175469),
+(1488, 1, 1, 24, 3, 113934),
+(1490, 1, 1, 25, 2, 142561),
+(1474, 1, 1, 26, 2, 59269),
+(1478, 1, 1, 27, 1, 85028),
+(1467, 1, 1, 28, 1, 49820),
+(1466, 1, 1, 29, 2, 107933),
+(1491, 1, 1, 30, 1, 42318),
+(1479, 1, 1, 31, 2, 51309),
+(1481, 1, 1, 32, 3, 70752),
+(1477, 1, 1, 33, 3, 75054),
+(1489, 1, 1, 34, 3, 65060),
+(1464, 1, 1, 35, 1, 54716),
+(1487, 1, 1, 36, 2, 55743),
+(1472, 1, 1, 37, 1, 63065),
+(1485, 1, 1, 38, 2, 53760),
+(1473, 1, 1, 39, 3, 39288),
+(1482, 1, 1, 40, 3, 52989),
+(1484, 1, 1, 41, 3, 43980),
+(1463, 1, 1, 42, 2, 58247),
+(1469, 1, 1, 43, 2, 41351),
+(1465, 1, 1, 44, 2, 37823),
+(1471, 1, 1, 45, 2, 68695),
+(1462, 1, 1, 46, 1, 58787),
+(1476, 1, 1, 47, 3, 14420),
+(1468, 1, 1, 48, 3, 25745),
+(1486, 1, 1, 49, 1, 22163),
+(1480, 1, 1, 50, 1, 8027),
+(1470, 1, 1, 51, 1, 19372);
 
 -- --------------------------------------------------------
 
@@ -205,48 +202,43 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_leftdate` bigint(20) DEFAULT NULL,
   `user_defaultteam` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=52 ;
 
 --
 -- Daten für Tabelle `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_entrydate`, `user_leftdate`, `user_defaultteam`) VALUES
-(1, 'Fox', 1445330202, NULL, 3),
-(2, 'Multitalent', 1445330202, NULL, 1),
-(3, 'Jule', 1445330202, NULL, 2),
-(4, 'g0k', 1445330202, NULL, 0),
-(5, 'Flo', 1445330202, NULL, 0),
-(6, 'heinreich', 1447923323, NULL, 0),
-(7, 'lenzer', 1447723348, 1447923348, 0),
-(8, 'blubber', 1446764400, NULL, 2);
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `userteam`
---
-
-CREATE TABLE IF NOT EXISTS `userteam` (
-  `userteam_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `userteam_user_id` int(10) unsigned NOT NULL,
-  `userteam_team` int(10) unsigned DEFAULT NULL,
-  `userteam_from` bigint(20) DEFAULT NULL,
-  `userteam_to` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`userteam_id`),
-  KEY `userteam_FKIndex1` (`userteam_user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
-
---
--- Daten für Tabelle `userteam`
---
-
-INSERT INTO `userteam` (`userteam_id`, `userteam_user_id`, `userteam_team`, `userteam_from`, `userteam_to`) VALUES
-(1, 1, 2, 1446591600, 1446937200),
-(2, 2, 3, NULL, NULL),
-(3, 3, 1, NULL, NULL),
-(4, 4, 1, NULL, NULL),
-(5, 5, 3, NULL, NULL);
+(22, 'gOk', 1446332400, NULL, 1),
+(23, 'multitalent74', 1446332400, NULL, 3),
+(24, 'Tito_sy', 1446332400, NULL, 3),
+(25, 'zappla', 1446332400, NULL, 2),
+(26, 'Fox84', 1446332400, NULL, 2),
+(27, 'Jule0777', 1446332400, NULL, 1),
+(28, 'cOnTiNuE83', 1446332400, NULL, 1),
+(29, 'Ch053n0n2', 1446332400, NULL, 2),
+(30, 'Zitarus', 1446332400, NULL, 1),
+(31, 'jurginho', 1446332400, NULL, 2),
+(32, 'm9k', 1446332400, NULL, 3),
+(33, 'Jaaaaa', 1446332400, NULL, 3),
+(34, 'Udo203', 1446332400, NULL, 3),
+(35, 'Bambis Mutter', 1446332400, NULL, 1),
+(36, 'Tieu', 1446332400, NULL, 2),
+(37, 'FdBk!', 1446332400, NULL, 1),
+(38, 'Rennsemmel', 1446332400, NULL, 2),
+(39, 'Flo H', 1446332400, NULL, 3),
+(40, 'Malochers', 1446332400, NULL, 3),
+(41, 'PI-Billy21', 1446332400, NULL, 3),
+(42, 'badboydiz', 1446332400, NULL, 2),
+(43, 'Don Mir', 1446332400, NULL, 2),
+(44, 'Blackbird26', 1446332400, NULL, 2),
+(45, 'Fandy2504', 1446332400, NULL, 2),
+(46, 'Akin Dogan', 1446332400, NULL, 1),
+(47, 'Groos', 1446332400, NULL, 3),
+(48, 'Dfelder', 1446332400, NULL, 3),
+(49, 'RoyJn', 1446332400, NULL, 1),
+(50, 'Kater cameo', 1446332400, NULL, 1),
+(51, 'Earth Guard', 1446332400, NULL, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
